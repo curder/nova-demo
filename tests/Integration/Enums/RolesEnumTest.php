9f7393f568032ecf64eee85a6452232942720b86
curder <curder@users.noreply.github.com>
2022-03-10 08:12:43 +0000

    Fix styling

--- tests/Integration/Enums/RolesEnumTest.php
+++ tests/Integration/Enums/RolesEnumTest.php
@@ -2,25 +2,24 @@
 
 use App\Enums\RolesEnum;
 
-it('has users static method on RolesEnum class', fn() => expect(RolesEnum::users(RolesEnum::getRandomValue()))
+it('has users static method on RolesEnum class', fn () => expect(RolesEnum::users(RolesEnum::getRandomValue()))
     ->toBeCollection());
 
-it('will return empty collection when use faker value for users static method', fn() => expect(RolesEnum::users('faker-value'))
+it('will return empty collection when use faker value for users static method', fn () => expect(RolesEnum::users('faker-value'))
     ->toBeCollection()
     ->toBeEmpty());
 
-it('has permissions static method', fn() => expect(RolesEnum::permissions(RolesEnum::getRandomValue()))
+it('has permissions static method', fn () => expect(RolesEnum::permissions(RolesEnum::getRandomValue()))
     ->toBeCollection());
 
-it('will return empty collection when use faker value for permissions static method', fn() => expect(RolesEnum::permissions('faker-value'))
+it('will return empty collection when use faker value for permissions static method', fn () => expect(RolesEnum::permissions('faker-value'))
     ->toBeCollection()
     ->toBeEmpty());
 
-it('has some dynamic static method #SUPERADMIN', fn() => expect(RolesEnum::SUPER_ADMIN())
+it('has some dynamic static method #SUPERADMIN', fn () => expect(RolesEnum::SUPER_ADMIN())
     ->toBeInstanceOf(RolesEnum::class)
     ->toBeEnum('SUPER_ADMIN', 'superAdmin', __('enums.App\Enums\RolesEnum.superAdmin')));
 
-it('has some dynamic static method #CONTENT', fn() => expect(RolesEnum::CONTENT())
+it('has some dynamic static method #CONTENT', fn () => expect(RolesEnum::CONTENT())
     ->toBeInstanceOf(RolesEnum::class)
     ->toBeEnum('CONTENT', 'content', __('enums.App\Enums\RolesEnum.content')));
-
