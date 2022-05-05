b0c36f1343c29925f2fa37563015f52687bb58b1
curder <q.curder@gmail.com>
2022-05-05 16:33:03 +0800

    Fixed phpstan style

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name', 'email', 'password',
@@ -39,7 +39,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for arrays.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $hidden = [
         'password', 'remember_token',
@@ -48,7 +48,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast to native types.
      *
-     * @var array
+     * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
