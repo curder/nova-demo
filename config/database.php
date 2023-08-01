00a2135a5e154ad28452057890fa98586cc32588
curder <q.curder@gmail.com>
2023-08-01 10:09:35 +0800

    Add dump_binary_path config for laravel-backup

--- config/database.php
+++ config/database.php
@@ -58,6 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'dump' => [
+                'dump_binary_path' => env('DUMP_BINARY_PATH', '/usr/local/mysql/bin'),
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
