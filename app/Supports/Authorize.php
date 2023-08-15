0f639778ac66bc4a5adc4a48f56d03fee7b5638b
curder <q.curder@gmail.com>
2023-08-15 19:38:01 +0800

    Refactor auth

--- app/Supports/Authorize.php
+++ app/Supports/Authorize.php
@@ -10,16 +10,21 @@ class Authorize
 {
     public static function canSeeMenus(): Closure
     {
-        return fn (Request $request) => $request->user()->hasPermissionTo(Enums\Permission::ManagerMenus->value);
+        return static::hasPermission(Enums\Permission::ManagerMenus);
     }
 
     public static function canSeeLogs(): Closure
     {
-        return fn (Request $request) => $request->user()->hasPermissionTo(Enums\Permission::ViewLogs->value);
+        return static::hasPermission(Enums\Permission::ViewLogs);
     }
 
     public static function canSeeBackups(): Closure
     {
         return fn (Request $request) => $request->user()->isSuperAdmin();
+    }
+
+    private static function hasPermission(Enums\Permission $enum): Closure
+    {
+        return fn (Request $request) => $request->user()->hasPermissionTo($enum->value);
     }
 }
