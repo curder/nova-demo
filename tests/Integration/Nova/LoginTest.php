233c598fe9fa8c56e63bff943169eb7dd691dd3b
curder <q.curder@gmail.com>
2023-08-18 19:27:32 +0800

    Add some test case for user login

---
+++ tests/Integration/Nova/LoginTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Database\Seeders\UserSeeder;
+use function Pest\Laravel\get;
+
+it('can redirect correct url when user login in', function () {
+    get('/cp/login')->assertOk();
+
+    $this->seed(UserSeeder::class);
+    $this->loginAsAdmin();
+
+    get('/cp/login')->assertRedirect('/cp');
+    get('/cp')->assertRedirect('cp/resources/users');
+});
